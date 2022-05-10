06f8464b5d7892e4e16cd1efa0f16b4cf4665de3
Shazzad <shazzad.cse.97@gmail.com>
2022-05-10 23:31:07 +0600

    Iron_Man V:C

---
+++ app/src/main/java/com/example/ironman_c/Login.java
@@ -0,0 +1,14 @@
+package com.example.ironman_c;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
